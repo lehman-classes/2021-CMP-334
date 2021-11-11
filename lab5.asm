5194ee0d7a208a996618c8cfd58c75c57b89f01c
Hendrix Tavarez <hendrix@lockerfish.com>
2021-11-10 20:47:34 -0500

    mips assembly stuff

--- lab5.asm
+++ lab5.asm
@@ -13,3 +13,35 @@
 # }
 #
 
+.data
+
+
+.text
+# int x = 1
+li $t0, 1
+# int y = 0
+addi $t1, $zero, 0
+
+# if (x == 0) {
+beqz $t0, add_one
+# } else if (x == 1) {
+beq $t0, 1, sub_one
+# } else {
+j set_one_hundred
+
+add_one:
+  # y++;
+  addi $t1, $t1, 1
+  j end_prog
+
+sub_one:
+  # y--;
+  subiu $t1, $t1, 1
+  j end_prog
+
+set_one_hundred:
+  li $t1, 100
+
+end_prog:
+  li $v0, 10
+  syscall
